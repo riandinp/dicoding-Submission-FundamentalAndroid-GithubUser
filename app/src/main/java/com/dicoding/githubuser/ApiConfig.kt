2611c65f2b57b0bbac9be1f94961650af96a9028
Riandi NP <riandi.np20@mhs.uinjkt.ac.id>
2023-03-23 19:55:38 +0700

    add Logging Interceptor

--- app/src/main/java/com/dicoding/githubuser/ApiConfig.kt
+++ app/src/main/java/com/dicoding/githubuser/ApiConfig.kt
@@ -3,17 +3,18 @@ package com.dicoding.githubuser
 import com.dicoding.githubuser.network.GithubApiService
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
     companion object{
         fun getApiService(): GithubApiService {
-//            val loggingInterceptor = if(BuildConfig.DEBUG) {
-//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//            } else {
-//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-//            }
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
 
             val authInterceptor = Interceptor { chain ->
                 val req = chain.request()
@@ -24,6 +25,7 @@ class ApiConfig {
             }
 
             val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
                 .addInterceptor(authInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
